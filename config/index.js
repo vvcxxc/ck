261cce35a71d994bc11c4ab6c1300bc927863299
xiezhiwei <18520568417@163.com>
2018-08-05 09:49:15 +0800

    modify

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: "192.168.2.189", // can be overwritten by process.env.HOST
+    host: "192.168.0.39", // can be overwritten by process.env.HOST
     // host: 'api.tdianyi.com', // can be overwritten by process.env.HOST
     port: 8080, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,

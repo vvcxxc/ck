0a2c025cbc59424a332edd468aa0749b3e36a31a
zhuwj <767839489@qq.com>
2019-01-03 16:11:50 +0800

    t

--- src/view/entrepreneur/entrepreneurs.vue
+++ src/view/entrepreneur/entrepreneurs.vue
@@ -6,20 +6,31 @@
       @pullingUp="hanleLoadMore"
     >
       <div class="container">
-        <c-list v-for="(item, index) in entrepreneurs" :key="index" :showOptions="{image: true}" :data="item"></c-list>
+        <c-list v-for="(item, index) in entrepreneurs" :key="index" :showOptions="{image: true}" :data="item" @on-click-button="onClickButton"></c-list>
       </div>
     </c-scroll-new>
+
+    <div v-transfer-dom>
+      <confirm
+        v-model="showModal"
+        show-input
+        :title="`分配积分`"
+        :input-attrs="{type: 'number'}"
+        @on-confirm="onConfirm"
+      />
+    </div>
   </div>
 </template>
 
 <script>
-  import { XHeader } from "vux"
+  import { XHeader, Confirm, TransferDomDirective as TransferDom } from "vux"
   import { mapGetters } from "vuex"
   import CScroll from "@components/c-scroll/scroll"
   import CList from "@components/c-list/list"
   import CScrollNew from "@components/c-n-scroll/scroll"
 
   import { entrepreneurs } from "@api/api"
+  import {giveIntegral} from "../../api/api";
 
   const REQUEST_ONE = 1
 
@@ -31,9 +42,14 @@
           account_name: "创客测试号",
           account_phone: "13800138000",
           account_regist_time: 1532068447,
-          party_id: 121
-        }
+          party_id: 121,
+        },
+        showModal: false,
+        currentId: 0,
       }
+    },
+    directives: {
+      TransferDom
     },
     computed: {
       ...mapGetters(['role_type'])
@@ -42,9 +58,23 @@
       XHeader,
       CScroll,
       CList,
-      CScrollNew
+      CScrollNew,
+      Confirm
     },
     methods: {
+      onClickButton(id) {
+        this.showModal = true;
+        this.currentId = id
+      },
+      async onConfirm(val) {
+        const { code, message } = await giveIntegral({ party_id: this.currentId, integral: val });
+        if (code === 200) {
+          this.$vux.toast.text(message);
+          this.fetchEntrepreneurs()
+        } else {
+          this.$vux.toast.text(message)
+        }
+      },
       hanleLoadMore () {
         window.setTimeout(() => {
           this.fetchEntrepreneurs()
@@ -72,7 +102,9 @@
               }
               return {
                 src: 'static/img/face2.png',
-                desc: output
+                desc: output,
+                id: item.party_id,
+                integral: item.integral.length > 0 ? item.integral[0]['integral'] : 0,
               }
             })
           ]

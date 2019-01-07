3a12781e884551f094c28fb1ff496630a03e67cb
zhuwj <767839489@qq.com>
2019-01-07 18:02:17 +0800

    t

--- config/release.env.js
+++ config/release.env.js
@@ -4,10 +4,10 @@ const devEnv = require("./dev.env")
 
 module.exports = merge(devEnv, {
   NODE_ENV: '"release"',
-  BASE_DOMAIN: '"http://release.api.supplier.tdianyi.com/"',
+  BASE_DOMAIN: '"http://test.api.supplier.tdianyi.com/"',
   // 上传图片
   UPLOAD_API: '"http://test.o2o.tdianyi.com/"',
-  DOMAIN: '"http://release.supplier.tdianyi.com"',
+  DOMAIN: '"http://test.supplier.tdianyi.com"',
 
   OSS_UPLOAD_URL: '"http://tmwl.oss-cn-shenzhen.aliyuncs.com"',
 })

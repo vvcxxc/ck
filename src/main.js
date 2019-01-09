8a53863a452fba925896bc74b9053da09ee568cf
zhuwj <767839489@qq.com>
2019-01-09 10:45:25 +0800

    t

--- src/main.js
+++ src/main.js
@@ -19,6 +19,39 @@ Vue.use(ToastPlugin)
 
 Vue.config.productionTip = false
 
+const isAndroid = navigator.userAgent.indexOf('Android') > -1 || navigator.userAgent.indexOf('Adr') > -1; //android终端
+Vue.directive('resetInput', {
+  // 当被绑定的元素插入到 DOM 中时……
+  inserted: function (el) {
+    if(isAndroid){
+      return;
+    }
+
+    let input = document.getElementById('reset-input');
+
+    if (!input) {
+      input = document.createElement('INPUT');
+      input.type = 'text';
+      input.style.height = '0px'
+      input.style.width = '0px'
+      input.style.position = 'absolute'
+      input.id = 'reset-input';
+      document.body.prepend(input);
+    }
+
+    el.addEventListener('blur',function(){
+      input.focus();
+      input.blur();
+    });
+
+    // console.log(el)
+    // el.addEventListener('click', function () {
+    //   input.focus();
+    //   input.blur();
+    // })
+  }
+});
+
 /* eslint-disable no-new */
 new Vue({
   el: "#app",

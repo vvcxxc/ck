8042313e82294bb33331b09948262f8f63fcc81f
zhuwj <767839489@qq.com>
2019-01-19 14:45:03 +0800

    t

--- src/components/c-list/list.vue
+++ src/components/c-list/list.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="list-wrapper">
+  <div class="list-wrapper" v-if="data.is_show">
     <div class="list-container">
       <div class="image" v-show="_showOptions.image">
         <img :src="data.src" alt="" width="56" height="56">

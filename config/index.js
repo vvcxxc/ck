0c81f7d5f76443c89c73105f5fec070fedea1519
songyizhe <769724661@qq.com>
2019-08-30 10:06:04 +0800

    改接口

--- config/index.js
+++ config/index.js
@@ -12,7 +12,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: "0.0.0.0", // can be overwritten by process.env.HOST
+    host: "localhost", // can be overwritten by process.env.HOST
     // host: 'api.tdianyi.com', // can be overwritten by process.env.HOST
     port: 8086, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,

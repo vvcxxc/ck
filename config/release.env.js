3662e013eb9473a550d713c8cee7b6c149feed7c
songyizhe <769724661@qq.com>
2019-09-06 11:45:30 +0800

    修改地址

--- config/release.env.js
+++ config/release.env.js
@@ -2,15 +2,15 @@
 const merge = require("webpack-merge")
 const devEnv = require("./dev.env")
 
-module.exports = merge(devEnv, { 
+module.exports = merge(devEnv, {
   NODE_ENV: '"release"',
-  BASE_DOMAIN: '"http://api.supplier.tdianyi.com/"',
+  BASE_DOMAIN: '"http://release.supplier.tdianyi.com/"',
   // 上传图片
   UPLOAD_API: '"http://o2o.tdianyi.com/"',
   DOMAIN: '"http://ck.tdianyi.com"',
 
   OSS_UPLOAD_URL: '"http://tmwl.oss-cn-shenzhen.aliyuncs.com"',
- 
+
   SUPPLIER_URL: '"http://supplier.tdianyi.com"',
- 
+
 })

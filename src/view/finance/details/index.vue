fb423d1312b148c367e2ed3b5d34a27695759428
songyizhe <769724661@qq.com>
2020-01-06 16:17:33 +0800

    组件的使用

--- src/view/finance/details/index.vue
+++ src/view/finance/details/index.vue
@@ -2,16 +2,36 @@
   <div class="details-page">
     <div class="details-header">
       <div>多美蛋糕店</div>
-      <div></div>
+      <div>
+
+      </div>
     </div>
+    <van-popup  position="bottom" v-model="show">
+      <van-datetime-picker
+          v-model="currentDate"
+          type="date"
+          :min-date="minDate"
+          :max-date="maxDate"
+        />
+    </van-popup>
   </div>
 </template>
 <script type="text/javascript">
+import { DatetimePicker,Popup  } from 'vant';
+import Vue from "vue";
+Vue.use(Popup)
+Vue.use(DatetimePicker);
 export default {
   data(){
     return {
-
+      minDate: new Date(2020, 0, 1),
+      maxDate: new Date(2025, 10, 1),
+      currentDate: new Date(),
+      show: true
     }
+  },
+  components: {
+    DatetimePicker
   }
 }
 </script>

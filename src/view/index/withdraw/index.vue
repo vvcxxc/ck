0753404de2125334966324f8cbd0596c24f394bc
xiqoqilin <w3c_2014@163.com>
2020-01-07 17:53:07 +0800

    提现

--- src/view/index/withdraw/index.vue
+++ src/view/index/withdraw/index.vue
@@ -8,28 +8,51 @@
           <p class="bank-num">尾号{{info.last_number}}储蓄卡</p>
         </div>
       </div>
-      <div class="header-r">
+      <!-- <div class="header-r">
         <img src="/static/img/right.png" />
-      </div>
+      </div>-->
     </div>
     <div class="main">
       <div class="main-title">提现金额</div>
       <div class="input-box">
         <span>￥</span>
-        <input type="number" v-model="money" />
+        <!-- <input type="number" v-model="money"/> -->
+        <van-cell-group style="width:100%;">
+          <van-field
+            type="number"
+            v-model="money"
+            placeholder="请输入要提现的金额"
+            clearable
+            style="font-size:24px"
+          />
+        </van-cell-group>
       </div>
-      <div class="withdraw_money">可用额度￥{{info.money}}</div>
+      <div class="money_info">
+        <div class="withdraw_money">
+          可提现金额
+          <span style="color: #000">￥{{info.money}}</span>
+        </div>
+        <div class="all_withdraw">全部提现</div>
+      </div>
     </div>
     <div class="button-box">
-      <div class="button" @click="withdraw">申请提现</div>
+      <!-- <div class="button" @click="withdraw">申请提现</div> -->
+      <van-button round type="info" @click="withdraw" class="button">提现</van-button>
     </div>
     <div class="tips">
       <div class="tips-title">温馨提示：</div>
-      <div class="tips-text">1、银行卡到账时间：T+1日</div>
+      <div class="tips-text">1、金额限制: 最低提现金额为100元。</div>
+      <div class="tips-text">2、银行卡到账时间: T+1（第二个工作日）。</div>
+      <div class="tips-text">3、提现进度查询位置: “首页-提现明细”</div>
+      <div class="tips-text">4、因网银转账的规则限制，提现金额小数仅能精确到小数点后2位。</div>
     </div>
   </div>
 </template>
 <script>
+import Vue from "vue";
+import { Field, Button } from "vant";
+Vue.use(Field);
+Vue.use(Button);
 import { withdrawInfo, withdrawApply } from "@api/api";
 export default {
   data() {
@@ -40,8 +63,8 @@ export default {
   },
   created() {
     withdrawInfo().then(res => {
-      console.log(res)
-      if(res.code == 200){
+      console.log(res);
+      if (res.code == 200) {
         this.info = res.data;
       }
     });
@@ -56,7 +79,6 @@ export default {
           withdrawApply(params)
             .then(({ message, code }) => {
               this.$vux.toast.text(message);
-
             })
             .catch(err => console.log(err));
         } else {

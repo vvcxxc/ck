ea0598320f3f02bad13598d04163da3fe64dc40b
songyizhe <769724661@qq.com>
2020-01-07 14:17:02 +0800

    收益列表页详情页

--- src/view/finance/details/index.vue
+++ src/view/finance/details/index.vue
@@ -1,77 +1,46 @@
 <template>
-  <div class="details-page">
-    <div class="details-header">
-      <div>多美蛋糕店</div>
-      <div @click="showDatePicker" class="date">
-        {{date}}
-        <van-icon name="arrow-down" class="icon"/>
+  <div class="page">
+    <div class="money">
+      +10086
+    </div>
+    <div class="title">费率返点</div>
+
+    <div class="layout-box">
+      <div class="layout-item">
+      <div>说明类型</div>
+      <div>费率返点</div>
+      </div>
+      <div class="layout-item">
+        <div>说明类型</div>
+        <div>费率返点</div>
+      </div>
+      <div class="layout-item">
+        <div>说明类型</div>
+        <div>费率返点</div>
       </div>
     </div>
 
-    <div class="total-money">
-      <div class="money">
-        ￥0.00
+    <div class="layout-item">
+        <div>说明类型</div>
+        <div>费率返点</div>
       </div>
-      <div class="name">
-        费率返点收益
+      <div class="layout-item">
+        <div>说明类型</div>
+        <div>费率返点</div>
       </div>
-    </div>
 
-    <div class="details-list">
-      <div class="title">费率返点明细</div>
-      <div>
-        <div class="list-item">
-          <div class="item-l">
-            <div class="store-name">多美蛋糕店</div>
-            <div class="date">2019/11/11</div>
-          </div>
-          <div class="item-r">
-            <div class="money">0.56</div>
-            <van-icon name="arrow" class="icon" />
-          </div>
-        </div>
-      </div>
-    </div>
-
-    <van-popup  position="bottom" v-model="show">
-      <van-datetime-picker
-          v-model="currentDate"
-          type="date"
-          @confirm='chooseDate'
-          @cancel='showDatePicker'
-        />
-    </van-popup>
   </div>
 </template>
-<script type="text/javascript">
-import { DatetimePicker, Popup, Icon } from 'vant';
-import Vue from "vue";
-import dayjs from 'dayjs'
-Vue.use(Popup)
-Vue.use(DatetimePicker)
-Vue.use(Icon);
+<script>
 export default {
   data(){
     return {
-      currentDate: new Date(),
-      show: false,
-      date: dayjs(new Date()).format('YYYY-MM-DD')
+
     }
   },
-  methods: {
-    // 展示隐藏日期选择器
-    showDatePicker(){
-      this.show = !this.show
-    },
-    // 选择时间
-    chooseDate(date){
-      let time = dayjs(date).format('YYYY-MM-DD')
-      this.show = false
-      this.date = time
-    }
-  }
+
 }
 </script>
 <style lang="sass" scoped>
- @import './index'
+  @import './index'
 </style>

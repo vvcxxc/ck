848cd4aa38f82a860b7a5ea5321991de27cacf89
songyizhe <769724661@qq.com>
2020-01-08 11:47:14 +0800

    test

--- src/view/index/president/president.vue
+++ src/view/index/president/president.vue
@@ -76,6 +76,7 @@ export default {
   computed: {},
   methods: {
     invite(name) {
+      console.log(name)
       if (name == "people") {
         let qrCodeUrl = `http://${window.location.host}/ck/register?invite_id=${this.info.party_id}`;
         this.qrCodeUrl = qrCodeUrl;

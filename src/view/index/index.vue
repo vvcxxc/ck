af3da156c36421e92c71fca382e7ef8f03b3bb52
songyizhe <769724661@qq.com>
2020-01-15 17:19:47 +0800

    a

--- src/view/index/index.vue
+++ src/view/index/index.vue
@@ -6,7 +6,7 @@
       <div class="header">
         <div class="header-l">
           <p class="header-l-title">余额</p>
-          <p class="header-l-money">88.00</p>
+          <p class="header-l-money">{{info.amount}}</p>
         </div>
         <div class="header-r">
           <p class="header-r-title">
@@ -20,25 +20,25 @@
       <div class="info">
         <div class="grid">
           <div class="item">
-            <p class="money">200.00</p>
+            <p class="money">{{info.today_fee}}</p>
             <p class="label">今日收益</p>
           </div>
-          <div class="item">
-            <p class="money">102</p>
+          <div class="item" >
+            <p class="money">{{info.supplier_number}}</p>
             <p class="label">邀请的店铺</p>
           </div>
-          <div class="item">
-            <p class="money">200</p>
+          <div class="item" >
+            <p class="money">{{info.entrepreneur_number}}</p>
             <p class="label">邀请的创客</p>
           </div>
         </div>
 
-        <div class="invite-box">
+        <div class="invite-box" @click="invite('store')">
           <div class="invite-botton">
             <i class="icon store" />
             <div>邀请店铺</div>
           </div>
-          <div class="invite-botton">
+          <div class="invite-botton" @click="invite('people')">
             <i class="icon people" />
             <div>邀请创客</div>
           </div>
@@ -57,43 +57,32 @@
 
         <div class="top-list">
           <van-list v-model="loading" :finished="finished" finished-text="没有更多了" @load="onLoad">
-            <div class="list-item">
-              <div class="item-name">杨大富</div>
-              <div class="item-phone">13078881111</div>
-              <div class="item-money">3214</div>
+            <div class="list-item" v-for="item in list" :key='item'>
+              <div class="item-name">{{item.account_name}}</div>
+              <div class="item-phone">{{item.account_phone}}</div>
+              <div class="item-money">{{item.money}}</div>
             </div>
           </van-list>
         </div>
       </div>
     </div>
+     <van-overlay :show="is_show" @click="is_show = false">
+      <div class="qr_code">
+        <div class="qr_code_title">{{title}}</div>
+        <img class="qr_code_img" :src="codeUrl" />
+      </div>
+    </van-overlay>
   </div>
 </template>
 <script>
-// import { Vue, Component } from "vue-property-decorator";
-// import { Getter } from "vuex-class";
-import { NavBar, Icon, List } from "vant";
+import { NavBar, Icon, List, Overlay } from "vant";
 import checkLogin from "@/decorator/check_login";
-import { getPeopleTopList, getStoreTopList } from "@api/api";
+import { getPeopleTopList, getStoreTopList, indexInfo } from "@api/api";
 import { mapGetters } from "vuex";
+import QRCode from "qrcode";
 const REQUEST_OK = 200;
 const ZERO = 0;
-// @Component({
-//   components: {
-//   }
-// })
-// export default class Index extends Vue {
-//   @Getter("role_type") getterRoleType;
-//   @checkLogin
-//   created() {
-//     const token = window.localStorage.getItem("token");
-//     if (!token) {
-//       this.$router.push("/login");
-//     }
-//   }
-//   tabClick = () => {
-//     console.log(this.$ref)
-//   }
-// }
+
 
 export default {
   data() {
@@ -101,7 +90,13 @@ export default {
       tab: 0,
       list: [],
       loading: false,
-      finished: false
+      finished: false,
+      page: 1,
+      info: {},
+      qrCodeUrl: "", // 二维码信息
+      codeUrl: "", //二维码图片路径
+      is_show: false, // 展示邀请页面
+      title: "", // 邀请时候的title
     };
   },
   computed: {
@@ -112,24 +107,62 @@ export default {
     if (!token) {
       this.$router.push("/login");
     }
-    this.getStoreTop()
+    indexInfo().then(res => {
+      console.log(res)
+      this.info = res.data
+    })
   },
   methods: {
+     invite(name) {
+      if (name == "people") {
+        let qrCodeUrl = `http://${window.location.host}/ck/register?invite_id=${this.info.party_id}`;
+        this.qrCodeUrl = qrCodeUrl;
+        this.showQRcode(qrCodeUrl);
+        this.title = "邀请创客";
+      } else {
+        let qrCodeUrl = `${process.env.SUPPLIER_URL}/register?invite_phone=${this.info.account_phone}`;
+        this.qrCodeUrl = qrCodeUrl;
+        this.showQRcode(qrCodeUrl);
+        this.title = "邀请店铺";
+      }
+    },
+    showQRcode(data) {
+      QRCode.toDataURL(data)
+        .then(url => {
+          this.codeUrl = url;
+          this.is_show = true;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    },
     tabClick(index) {
       // index=0 为创客排行榜 =1为店铺排行榜
       this.tab = index;
+      this.list = []
+      this.page = 1
+      if(this.tab){
+        this.getStoreTop()
+      }else{
+        this.getPeopleTop()
+      }
     },
     onLoad() {
       // 异步更新数据
       this.finished = true;
+      if(this.tab){
+        this.getStoreTop()
+      }else{
+        this.getPeopleTop()
+      }
     },
     // 获取创客排行榜
     getPeopleTop (){
-      let page = 1
-      getPeopleTopList(page).then(res => {
+      getPeopleTopList(this.page).then(res => {
         console.log(res)
         this.loading = false;
-        this.list = res.data
+        this.list =[ ...this.list ,...res.data]
+        this.page = this.page + 1
         if(res.pagination.current_page == res.pagination.total_pages || res.pagination.total == null){
           this.finished = true
         }
@@ -137,11 +170,11 @@ export default {
     },
     // 获取店铺排行榜
     getStoreTop (){
-      let page = 1
-      getStoreTopList(page).then(res => {
+      getStoreTopList(this.page).then(res => {
         console.log(res)
-         this.loading = false;
-        this.list = res.data
+        this.loading = false;
+        this.list =[ ...this.list ,...res.data]
+        this.page = this.page + 1
         if(res.pagination.current_page == res.pagination.total_pages || res.pagination.total == null){
           this.finished = true
         }

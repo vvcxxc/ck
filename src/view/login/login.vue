7adf73c2abc1e24d2fdff7df4ddf92c2ae5b9a10
vvcxxc <asd123>
2020-02-10 09:08:58 +0800

    提交

--- src/view/login/login.vue
+++ src/view/login/login.vue
@@ -21,7 +21,7 @@
           <input class="box_input" placeholder="请输入密码" type="password" v-model="account_passwd" />
         </div>
         <div class="login_box" @click="handleLogin">登录</div>
-        <!-- <div class="resign_box" v-if="role_type=='entrepreneur'" @click="goToRegister">注册</div> -->
+        <div class="resign_box" v-if="role_type=='entrepreneur'" @click="goToRegister">注册</div>
       </div>
     </div>
   </div>

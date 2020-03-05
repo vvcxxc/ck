4beefc6a0604d53c21746a3c1bc7108ec99cd660
songyizhe <769724661@qq.com>
2020-03-05 11:55:19 +0800

    完善资料接口修改

--- src/view/completeInformation/validate.js
+++ src/view/completeInformation/validate.js
@@ -4,13 +4,13 @@ let data = {
   party_id: 'party_id',
   identity_card_positive: '身份证正面照',
   identity_card_opposite: '身份证反面照',
-  hand_identity_card: '手持身份证照',
-  name: '姓名',
+  identity_hand_card: '手持身份证照',
+  identity_name: '姓名',
   identity_card: '身份证号不能为空',
-  validity_card: '身份证有效期',
-  positive: '银行卡正面',
-  opposite: '银行卡反面',
-  bank_account_user_name: '开户人',
+  identity_validity_card: '身份证有效期',
+  bank_positive: '银行卡正面',
+  bank_opposite: '银行卡反面',
+  bank_account_name: '开户人',
   bank_card_number: '银行卡号',
   bank_name: '开户行',
   bank_branch: '支行名称'
@@ -28,7 +28,7 @@ export default function Validate(info) {
     }
   }
   let validator = new Validator();
-  validator.add(info.name, [{
+  validator.add(info.identity_name, [{
     validateRule: 'isChinese',
     errmsg: '请输入正确的名字'
   }])

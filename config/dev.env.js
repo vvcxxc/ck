e64ad6756e6d00df56e4340db04f8d9f75a0e5a9
songyizhe <769724661@qq.com>
2020-04-01 10:21:16 +0800

    fix: 状态判断

--- config/dev.env.js
+++ config/dev.env.js
@@ -4,9 +4,9 @@ const prodEnv = require("./prod.env")
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
-  BASE_DOMAIN: '"http://test.api.supplier.tdianyi.com/"',
+  // BASE_DOMAIN: '"http://test.api.supplier.tdianyi.com/"',
   // BASE_DOMAIN: '"http://release.api.supplier.tdianyi.com/"',
-  // BASE_DOMAIN: '"http://api.supplier.tdianyi.com/"',
+  BASE_DOMAIN: '"http://api.supplier.tdianyi.com/"',
   // BASE_DOMAIN: '"http://release.api.supplier.com/"',
 
   SUPPLIER_URL: '"http://test.supplierv2.tdianyi.com"',

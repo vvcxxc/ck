d5d87fb1be5c7289114b7202328deca4f8bd6caa
songyizhe <769724661@qq.com>
2020-04-01 10:22:21 +0800

    fix: 状态判断

--- src/view/submitQua/result/index.vue
+++ src/view/submitQua/result/index.vue
@@ -5,12 +5,12 @@
       <img src="../../../assets/error.png" v-if="is_sq == 2 || 3 || 4 ||0" />
     </div>
     <div class="result-text" v-if="is_sq == 0">资料审核失败,请重新修改</div>
-    <div class="result-text" v-if="is_sq == 2 || 3 || 4">资料提交审核,请等候</div>
+    <div class="result-text" v-if="is_sq == 2 || is_sq ==3 || is_sq ==4">资料提交审核,请等候</div>
     <div class="result-text" v-if="is_sq == 1">资料审核通过</div>
 
     <!-- 按钮 -->
     <div class="button" @click='submit' v-if="is_sq == 0">重新提交</div>
-    <div class="button" @click='submit' v-if="is_sq == 2 || 3 || 4">知道了</div>
+    <div class="button" @click='submit' v-if="is_sq == 2 || is_sq ==3 || is_sq ==4">知道了</div>
     <div class="button" @click='submit' v-if="is_sq == 1">下一步</div>
   </div>
 </template>

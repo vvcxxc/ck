355f766a641d1001bd86a9d70a76fc8bf23759af
vvcxxc <asd123>
2020-04-15 15:27:09 +0800

    feat:资讯入口

--- src/view/my/component/personalCenterGird/index.vue
+++ src/view/my/component/personalCenterGird/index.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="list_gird" @click="onClick">
     <div class="icon_box">
-      <span :class="['small_bank_card','small_gift','small_id_card','small_set_up'][_props.type]"/>
+      <span
+        :class="['small_bank_card','small_gift','small_id_card','small_set_up','actual_combat'][_props.type]"
+      />
       {{_props.leftName}}
     </div>
     <div>
@@ -17,27 +19,28 @@ export default {
   data() {
     return {
       list: "",
-      type:['small_bank_card','small_gift','small_id_card','small_set_up']
+      // type: [
+      //   "small_bank_card",
+      //   "small_gift",
+      //   "small_id_card",
+      //   "small_set_up",
+      //   "actual_combat"
+      // ]
     };
   },
   props: {
-    leftName:String,
-    rightName:String,
-    type:String
+    leftName: String,
+    rightName: String,
+    type: String
   },
-  components: {
-    
-  },
-  mounted(){
-    
-  },
-  created() {
-  },
+  components: {},
+  mounted() {},
+  created() {},
   methods: {
     //跳转我的银行卡页面
-    onClick(){
-      this.$emit('onClick');
-      console.log('3333')
+    onClick() {
+      this.$emit("onClick");
+      console.log("3333");
       // props.onClick()
       // this.$router.push({ path: "/my/card_new" })
     }
@@ -45,5 +48,5 @@ export default {
 };
 </script>
 <style lang="scss" scoped>
-  @import "./index.scss";
+@import "./index.scss";
 </style>

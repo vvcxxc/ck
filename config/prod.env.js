8ce7d75cf9905f1ed1e047aeff29ef70a8e1904a
songyizhe <769724661@qq.com>
2020-04-17 16:41:56 +0800

    chore

--- config/prod.env.js
+++ config/prod.env.js
@@ -2,7 +2,8 @@
 module.exports = {
   NODE_ENV: '"production"',
   BASE_DOMAIN: '"http://api.supplier.tdianyi.com/"',
-
+  ARTICLE_DOMAIN: '"http://release.api.counsel_terrace.tdianyi.com/"',
+  INFORMATION_URL: '"http://release.information_center.tdianyi.com/"',
   SUPPLIER_URL: '"http://supplier.tdianyi.com"'
 
-} 
+}
